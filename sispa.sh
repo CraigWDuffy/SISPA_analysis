2720de65551be0d7f71b25ad18ab19a80692dd64
CraigWDuffy <58182380+CraigWDuffy@users.noreply.github.com>
2023-10-27 14:47:50 +0100

    Update sispa.sh

--- sispa.sh
+++ sispa.sh
@@ -128,13 +128,13 @@ if [[ -v consensus ]]; then
 		samtools faidx $consensus
 		refIndex=${refIndex/.fasta}
 		refIndex=${refIndex/.fa}
-		minimap2 -d $workingFolder/$refIndex.mmi $consensus -t 56
-		minimap2 $workingFolder/$refIndex.mmi -at 56 $j | samtools view -bT $consensus -@ 56 -o $workingFolder/$k.bam
-		samtools sort -@ 56 $workingFolder/$k.bam -o $workingFolder/$k.sorted.bam
-		rm -rf $working/$k.bam
-		bcftools mpileup -Ou -f $consensus $workingFolder/$k.sorted.bam --threads 56 --annotate FORMAT/AD,INFO/AD | bcftools call -mv -Oz --ploidy 2 --threads 56 > $workingFolder/$k.vcf.gz
-		bcftools index $workingFolder/$k.vcf.gz
-		bcftools consensus -f $consensus -I --mark-ins lc -o $workingFolder/$k.consensus.fasta $workingFolder/$k.vcf.gz
+		#minimap2 -d $workingFolder/$refIndex.mmi $consensus -t 56
+		#minimap2 $workingFolder/$refIndex.mmi -at 56 $j | samtools view -bT $consensus -@ 56 -o $workingFolder/$k.bam
+		#samtools sort -@ 56 $workingFolder/$k.bam -o $workingFolder/$k.sorted.bam
+		#rm -rf $working/$k.bam
+		#bcftools mpileup -Ou -f $consensus $workingFolder/$k.sorted.bam --threads 56 --annotate FORMAT/AD,INFO/AD | bcftools call -mv -Oz --ploidy 2 --threads 56 > $workingFolder/$k.vcf.gz
+		#bcftools index $workingFolder/$k.vcf.gz
+		#bcftools consensus -f $consensus -I --mark-ins lc -o $workingFolder/$k.consensus.fasta $workingFolder/$k.vcf.gz
 		Rscript consensus_plots.r $workingFolder
 	done
 fi
