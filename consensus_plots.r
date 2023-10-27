e2dfc9f8610167d91a105d5ef7616444e5f6bf05
CraigWDuffy <58182380+CraigWDuffy@users.noreply.github.com>
2023-10-27 15:15:47 +0100

    Update consensus_plots.r

--- consensus_plots.r
+++ consensus_plots.r
@@ -31,10 +31,10 @@ for (i in alldepths){
 	#print(i)
 	#print(currwf)
 	data=read.table(paste(currwf,"/",i,sep=""))
-	tiff(paste(i,".genomeCov.tiff",sep=""), width=3000, height=2000, units="px",res=300, compression="lzw")
+	tiff(paste(currwf,"/",i,".genomeCov.tiff",sep=""), width=3000, height=2000, units="px",res=300, compression="lzw")
 	plot(data$V2, data$V3, cex=0.5, pch=19)
 	dev.off()
-	tiff(paste(i,".genomeCov.rollmean100.tiff",sep=""), width=3000, height=2000, units="px",res=300, compression="lzw")
+	tiff(paste(currwf,"/",i,".genomeCov.rollmean100.tiff",sep=""), width=3000, height=2000, units="px",res=300, compression="lzw")
 	plot(rollmean(data$V3, 100), cex=0.5, pch=19)
 	dev.off()
 }
